92a0c7d057299b5c7aa8ba9c04a01f250be1322e
baliyankartik <kartikbaliyan1@gmail.com>
2025-01-12 13:57:15 +0530

    Initial commit

---
+++ src/main/java/com/cafe/coffee/cafe/CoffeeCafeManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cafe.coffee.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeCafeManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeCafeManagmentSystemApplication.class, args);
+	}
+
+}
